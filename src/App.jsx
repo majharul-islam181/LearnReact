7c5f4edfe3f93fd2e2dc3e6e86f89987220a6807
majharul181 <majharul.islam03@northsouth.edu>
2024-03-20 23:36:05 +0600

    learingReact

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import Temp1 from "./components/Temp1";
+
+const LoginStatusBtn =(status)=>{
+  if(status){
+    return <button>Login btn</button>
+
+  } else{
+     return <button>Logout btn</button>
+
+  }
+}
+
+
+const App = () => {
+
+  let marks = 9 ;
+  const city = ["Dhaka", "Bogura", "khulna"];
+  let ternaryOperator = true;
+  let conditonalRendaring = true;
+  return (
+    <div>
+
+      <Temp1/>
+
+
+
+{/* Immediatley Invoked function */}
+      {(
+        ()=>{
+          if(marks >= 90 && marks<=100){
+            return <h1>A+</h1>
+          }
+          else if(marks <=60){
+            return <h1>Fail</h1>
+          }
+        }
+      )()}
+
+{/* JCX Loop Inside */}
+          <ol>
+            {
+              city.map((items, i)=>{
+                return <li key={i.toString}>
+                  {items}
+                </li>
+
+              })
+            } 
+          </ol>
+
+          <p>Login Status : </p>
+
+          {LoginStatusBtn(true)}
+
+
+
+          {/* Ternary operator */}
+
+          {
+            ternaryOperator ? <h1> TernaryOperator is false</h1> : <h1>TernaryOperator is true</h1>
+          }
+
+
+          {
+            
+          }
+
+
+
+
+
+
+     
+      
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Temp1.jsx
@@ -0,0 +1,11 @@
+
+const Temp1 = () => {
+    return (
+        <div>
+            Tmp1
+            
+        </div>
+    );
+};
+
+export default Temp1;
